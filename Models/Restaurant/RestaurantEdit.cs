1728fdbba7267a2c11c8458f29362e4269f12233
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-02 18:16:58 -0500

    "Create Restaurant Edit Model"

---
+++ Models/Restaurant/RestaurantEdit.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Restaurant
+{
+    public class RestaurantEdit
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Location { get; set; } = string.Empty;
+    }
+}
