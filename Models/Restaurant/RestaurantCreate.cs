a3154d81c1b32eec6776f90d223363c9656edaf4
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-02 15:24:10 -0500

    "RestaurantCreate display model"

---
+++ Models/Restaurant/RestaurantCreate.cs
@@ -0,0 +1,20 @@
+using System.Security.AccessControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SystemAcl.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Restaurant
+{
+    public class RestaurantCreate
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; } = string.Empty;
+    }
+}
