94f63bd34c28b246249c8931ef9a01d9ff245d35
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-03 09:52:05 -0500

    "Add members to our IRatingService interface"

--- Services/Ratings/IRatingService.cs
+++ Services/Ratings/IRatingService.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestaurantRaterMVC.Models.Rating;
 
 namespace RestaurantRaterMVC.Services.Ratings
 {
     public class IRatingService
     {
-        
+        Task<List<RatingListItem>> GetRatingsAsync();
+        Task<List<RatingListItem>> GetRestaurantRatingsAsync(int restaurantId); 
     }
 }
