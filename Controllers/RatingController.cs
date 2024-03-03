8c25a0e8792a5957a75a36f72f68b4392899c818
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-03 10:09:40 -0500

    "Add Rating ListItem to View"

--- Controllers/RatingController.cs
+++ Controllers/RatingController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantRaterMVC.Services.Ratings;
+using RestaurantRaterMVC.Models.Rating;
 
 namespace RestaurantRaterMVC.Controllers
 {
@@ -12,7 +13,13 @@ namespace RestaurantRaterMVC.Controllers
         private readonly IRatingService _service;
         public RatingController(IRatingService service)
         {
-        _service = service;
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RatingListItem> model = await _service.GetRatingsAsync();
+            return View(model);
         }
     }
 }
