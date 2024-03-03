ee2fb4dd9210a3c7d802d88414607106eefc446d
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-03 16:41:10 -0500

    "Add Rating Create Model"

---
+++ Models/Rating/RatingCreate.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterMVC.Models.Rating
+{
+    public class RatingCreate
+    {
+    [Required]
+    [Display(Name = "Restaurant")]
+    public int RestaurantId { get; set; }
+
+    [Required, Range(1, 5)]
+    [Display(Name = "Rating")]
+    public double Score { get; set; }
+    }
+}
